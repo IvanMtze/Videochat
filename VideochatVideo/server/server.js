da03ea32fc21220fc58e1578d1fb091e0a7cac42
gerardo espinosa <ivanmtze96@gmail.com>
2021-05-25 11:23:35 -0500

    ok

--- VideochatVideo/server/server.js
+++ VideochatVideo/server/server.js
@@ -15,8 +15,8 @@ const { v4: uuidV4 } = require('uuid')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+app.get('/uuid', (req, res) => {
+  res.json({'uuid':`${uuidV4()}`})
 })
 
 app.get('/:room', (req, res) => {
@@ -25,9 +25,22 @@ app.get('/:room', (req, res) => {
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
+    //Connect to room
     socket.join(roomId)
     socket.to(roomId).broadcast.emit('user-connected', userId)
     console.log("user joined")
+
+	/*
+    * Send a message to room
+    */
+    socket.on('message', (message) => {
+        console.log(`message:  ${message}`);
+        socket.to(roomId).broadcast.emit('message',message);
+    });
+
+    /*
+    * Someone has disconnected from room
+    */
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
